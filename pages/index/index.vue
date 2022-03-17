980518329431ec728c3ba027261f0e5383cfbd3a
清风 <m15090054259@163.com>
2022-03-17 10:34:38 +0800

    初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,75 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">
+				uView - 多平台快速开发的UI框架 {{vuex_version}}
+				uView - 多平台快速开发的UI框架 {{vuex_user.name}}
+			</text>
+		</view>
+		<view class="button-demo">
+			<u-button @click="modifyVuex">按钮组件演示</u-button>
+		</view>
+		<view class="link-demo">
+			<u-link :color="$u.color['primary']" :under-line="true" href="http://www.uviewui.com">
+				跳转uView文档：www.uviewui.com</u-link>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			modifyVuex() {
+				this.$u.vuex('vuex_version', '1.0.3');
+				// 修改对象的形式，中间用"."分隔
+				this.$u.vuex('vuex_user.name', '诗圣1');
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		padding: 40rpx;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 100rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 28rpx;
+		color: $u-content-color;
+	}
+
+	.button-demo {
+		margin-top: 80rpx;
+	}
+
+	.link-demo {
+		margin-top: 80rpx;
+	}
+</style>
